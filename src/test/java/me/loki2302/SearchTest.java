2e6426fcc14ec4926c500b379a572b86af54e80f
loki2302 <loki2302@loki2302.me>
2015-02-13 11:36:21 +0300

    Cleaning up

--- src/test/java/me/loki2302/SearchTest.java
+++ src/test/java/me/loki2302/SearchTest.java
@@ -2,8 +2,6 @@ package me.loki2302;
 
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.aggregations.AggregationBuilders;
-import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,7 +10,7 @@ import java.io.IOException;
 import static org.junit.Assert.assertEquals;
 
 public class SearchTest extends ElasticSearchTest {
-    // TODO: split into individual tests
+    // TODO: extend the tests, add more specific checks
 
     @Before
     public void populate() throws IOException {
@@ -20,58 +18,73 @@ public class SearchTest extends ElasticSearchTest {
     }
 
     @Test
-    public void canSearchEmployees() throws IOException, InterruptedException {
-        SearchResponse searchByLastNameTermQueryResponse = client.prepareSearch("megacorp").setTypes("employee")
+    public void canSearchEmployeesWithLastNameTermQuery() {
+        SearchResponse searchResponse = client.prepareSearch("megacorp").setTypes("employee")
                 // TODO: quite weird. When I save a value like "Hello-There",
                 // TODO: the index is built for "hello" and "there",
                 // TODO: so in case of "Smith", I only can search by "smith" :-/
                 .setQuery(QueryBuilders.termQuery("lastName", "smith"))
                 .execute().actionGet();
-        assertEquals(2, searchByLastNameTermQueryResponse.getHits().totalHits());
+        assertEquals(2, searchResponse.getHits().totalHits());
+    }
 
-        SearchResponse searchByLastNameMatchQueryResponse = client.prepareSearch("megacorp").setTypes("employee")
+    @Test
+    public void canSearchEmployeesWithLastNameMatchQuery() {
+        SearchResponse searchResponse = client.prepareSearch("megacorp").setTypes("employee")
                 // as opposed to termQuery, this will first convert "Smith" to "smith"
                 .setQuery(QueryBuilders.matchQuery("lastName", "Smith"))
                 .execute().actionGet();
-        assertEquals(2, searchByLastNameMatchQueryResponse.getHits().totalHits());
+        assertEquals(2, searchResponse.getHits().totalHits());
+    }
 
-        SearchResponse searchByAgeTermQueryResponse = client.prepareSearch("megacorp").setTypes("employee")
+    @Test
+    public void canSearchEmployeesWithAgeTermQuery() {
+        SearchResponse searchResponse = client.prepareSearch("megacorp").setTypes("employee")
                 .setQuery(QueryBuilders.termQuery("age", 35))
                 .execute().actionGet();
-        assertEquals(1, searchByAgeTermQueryResponse.getHits().totalHits());
+        assertEquals(1, searchResponse.getHits().totalHits());
+    }
 
-        SearchResponse searchByAgeRangeQueryResponse = client.prepareSearch("megacorp").setTypes("employee")
+    @Test
+    public void canSearchEmployeesWithAgeRangeQuery() {
+        SearchResponse searchResponse = client.prepareSearch("megacorp").setTypes("employee")
                 .setQuery(QueryBuilders.rangeQuery("age").from(32))
                 .execute().actionGet();
-        assertEquals(2, searchByAgeRangeQueryResponse.getHits().totalHits());
+        assertEquals(2, searchResponse.getHits().totalHits());
+    }
 
-        SearchResponse searchByAboutMatchQueryResponse = client.prepareSearch("megacorp").setTypes("employee")
+    @Test
+    public void canSearchEmployeesWithAboutMatchQuery() {
+        SearchResponse searchResponse = client.prepareSearch("megacorp").setTypes("employee")
                 // this will look for "rock" and "climbing", not for "rock climbing"
                 .setQuery(QueryBuilders.matchQuery("about", "rock climbing"))
                 .execute().actionGet();
-        assertEquals(2, searchByAboutMatchQueryResponse.getHits().totalHits());
+        assertEquals(2, searchResponse.getHits().totalHits());
+    }
 
-        SearchResponse searchByAboutMatchPhraseQueryResponse = client.prepareSearch("megacorp").setTypes("employee")
+    @Test
+    public void canSearchEmployeesWithAboutMatchPhraseQuery() {
+        SearchResponse searchResponse = client.prepareSearch("megacorp").setTypes("employee")
                 // as opposed to matchQuery, this will look for "rock climbing"
                 .setQuery(QueryBuilders.matchPhraseQuery("about", "rock climbing"))
                 .execute().actionGet();
-        assertEquals(1, searchByAboutMatchPhraseQueryResponse.getHits().totalHits());
+        assertEquals(1, searchResponse.getHits().totalHits());
+    }
 
-        SearchResponse searchByOneInterestTermQueryResponse = client.prepareSearch("megacorp").setTypes("employee")
+    @Test
+    public void canSearchEmployeesWithOneInterestTermQuery() {
+        SearchResponse searchResponse = client.prepareSearch("megacorp").setTypes("employee")
                 .setQuery(QueryBuilders.termQuery("interests", "music"))
                 .execute().actionGet();
-        assertEquals(2, searchByOneInterestTermQueryResponse.getHits().totalHits());
+        assertEquals(2, searchResponse.getHits().totalHits());
+    }
 
-        SearchResponse searchByTwoInterestsTermQueryResponse = client.prepareSearch("megacorp").setTypes("employee")
+    @Test
+    public void canSearchEmployeesWithTwoInterestTermsQuery() {
+        SearchResponse searchResponse = client.prepareSearch("megacorp").setTypes("employee")
                 // at least one of "music" and "sports", not necessarily both
                 .setQuery(QueryBuilders.termsQuery("interests", "music", "sports"))
                 .execute().actionGet();
-        assertEquals(2, searchByTwoInterestsTermQueryResponse.getHits().totalHits());
-
-        SearchResponse allInterestsAggregationResponse = client.prepareSearch("megacorp").setTypes("employee")
-                .addAggregation(AggregationBuilders.terms("allInterests").field("interests"))
-                .execute().actionGet();
-        StringTerms allInterestsStringTerms = allInterestsAggregationResponse.getAggregations().get("allInterests");
-        assertEquals(3, allInterestsStringTerms.getBuckets().size());
+        assertEquals(2, searchResponse.getHits().totalHits());
     }
 }
