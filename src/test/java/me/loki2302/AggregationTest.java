ef10c9206062e4a58486ecb1a2a4ba72cc08a478
loki2302 <loki2302@loki2302.me>
2015-02-14 09:52:38 +0300

    Added average age aggregation test

--- src/test/java/me/loki2302/AggregationTest.java
+++ src/test/java/me/loki2302/AggregationTest.java
@@ -3,6 +3,7 @@ package me.loki2302;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
+import org.elasticsearch.search.aggregations.metrics.avg.Avg;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,15 +19,24 @@ public class AggregationTest extends ElasticSearchTest {
 
     @Test
     public void canGetAllInterests() {
-        SearchResponse allInterestsAggregationResponse = client.prepareSearch("megacorp").setTypes("employee")
+        SearchResponse searchResponse = client.prepareSearch("megacorp").setTypes("employee")
                 .addAggregation(AggregationBuilders.terms("allInterests").field("interests"))
                 .execute().actionGet();
-        StringTerms allInterestsStringTerms = allInterestsAggregationResponse.getAggregations().get("allInterests");
+        StringTerms allInterestsStringTerms = searchResponse.getAggregations().get("allInterests");
         assertEquals(3, allInterestsStringTerms.getBuckets().size());
+    }
+
+    @Test
+    public void canGetAverageAge() {
+        SearchResponse searchResponse = client.prepareSearch("megacorp").setTypes("employee")
+                .addAggregation(AggregationBuilders.avg("averageAge").field("age"))
+                .execute().actionGet();
+        Avg averageAge = searchResponse.getAggregations().get("averageAge");
+        assertEquals(30.66, averageAge.getValue(), 0.01); // 30.6666666666666....
     }
 
     // +TODO: get all tags
     // TODO: get all tags with person count per each tag
     // TODO: get min age and the related person
-    // TODO: get avg age
+    // +TODO: get avg age
 }
