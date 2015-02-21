0097ccdb4462dbd007b590f2b28743e5bf15a7c4
loki2302 <loki2302@loki2302.me>
2015-02-21 13:55:53 +0300

    Cleaning up CompletionSuggesterTest

--- src/test/java/me/loki2302/CompletionSuggesterTest.java
+++ src/test/java/me/loki2302/CompletionSuggesterTest.java
@@ -56,39 +56,38 @@ public class CompletionSuggesterTest extends ElasticSearchTest {
                 .setRefresh(true)
                 .execute().actionGet();
 
-        if(true) {
-            SuggestResponse suggestResponse = client.prepareSuggest("devices")
-                    .addSuggestion(SuggestBuilders.completionSuggestion("nameSuggest")
-                            .field("name")
-                            .text("i"))
-                    .execute().actionGet();
+        checkSuggestions("i", new String[] {"ipad", "iphone"});
+        checkSuggestions("ip", new String[] {"ipad", "iphone"});
+        checkSuggestions("ipa", new String[] {"ipad"});
+        checkSuggestions("ipad", new String[] {"ipad"});
+        checkSuggestions("iph", new String[] {"iphone"});
+        checkSuggestions("ipho", new String[] {"iphone"});
+        checkSuggestions("iphon", new String[] {"iphone"});
+        checkSuggestions("iphone", new String[] {"iphone"});
+        checkSuggestions("and", new String[] {"android"});
+    }
 
-            Suggest.Suggestion suggestion = suggestResponse.getSuggest().getSuggestion("nameSuggest");
-            List<Suggest.Suggestion.Entry> entries = suggestion.getEntries();
-            assertEquals(1, entries.size()); // TODO: what are these entries?
+    private void checkSuggestions(String searchText, String[] expectedOptions) {
+        SuggestResponse suggestResponse = client.prepareSuggest("devices")
+                .addSuggestion(SuggestBuilders.completionSuggestion("nameSuggest")
+                        .field("name")
+                        .text(searchText))
+                .execute().actionGet();
 
-            Suggest.Suggestion.Entry entry = entries.get(0);
-            List<Suggest.Suggestion.Entry.Option> options = entry.getOptions();
-            assertEquals(2, options.size());
-            assertEquals("ipad", options.get(0).getText().string());
-            assertEquals("iphone", options.get(1).getText().string());
-        }
+        Suggest.Suggestion suggestion = suggestResponse.getSuggest().getSuggestion("nameSuggest");
+        List<Suggest.Suggestion.Entry> entries = suggestion.getEntries();
+        assertEquals(1, entries.size());
 
-        if(true) {
-            SuggestResponse suggestResponse = client.prepareSuggest("devices")
-                    .addSuggestion(SuggestBuilders.completionSuggestion("nameSuggest")
-                            .field("name")
-                            .text("and"))
-                    .execute().actionGet();
+        Suggest.Suggestion.Entry entry = entries.get(0);
+        List<Suggest.Suggestion.Entry.Option> options = entry.getOptions();
 
-            Suggest.Suggestion suggestion = suggestResponse.getSuggest().getSuggestion("nameSuggest");
-            List<Suggest.Suggestion.Entry> entries = suggestion.getEntries();
-            assertEquals(1, entries.size()); // TODO: what are these entries?
+        int expectedNumberOfOptions = expectedOptions.length;
+        assertEquals(expectedNumberOfOptions, options.size());
 
-            Suggest.Suggestion.Entry entry = entries.get(0);
-            List<Suggest.Suggestion.Entry.Option> options = entry.getOptions();
-            assertEquals(1, options.size());
-            assertEquals("android", options.get(0).getText().string());
+        for(int i = 0; i < expectedNumberOfOptions; ++i) {
+            String expectedOption = expectedOptions[i];
+            String actualOption = options.get(i).getText().string();
+            assertEquals(expectedOption, actualOption);
         }
     }
 }
