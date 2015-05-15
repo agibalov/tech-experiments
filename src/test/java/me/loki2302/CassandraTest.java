5b07cb531f466926a2d6bb2816e6c3aec7ef9bd0
loki2302 <loki2302@loki2302.me>
2015-05-15 08:27:00 +0300

    Added a dummy MappingManager test

--- src/test/java/me/loki2302/CassandraTest.java
+++ src/test/java/me/loki2302/CassandraTest.java
@@ -4,6 +4,12 @@ import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.mapping.Mapper;
+import com.datastax.driver.mapping.MappingManager;
+import com.datastax.driver.mapping.annotations.Column;
+import com.datastax.driver.mapping.annotations.PartitionKey;
+import com.datastax.driver.mapping.annotations.Table;
+import com.google.common.base.Objects;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,8 +17,6 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-
-// TODO: try http://docs.datastax.com/en/developer/java-driver/2.1/java-driver/reference/crudOperations.html
 
 public class CassandraTest {
     private Cluster cluster;
@@ -57,5 +61,66 @@ public class CassandraTest {
 
         assertEquals(222, rows.get(1).getInt("note_id"));
         assertEquals("second note", rows.get(1).getString("content"));
+    }
+
+    @Test
+    public void canSaveAndLoadAFewRowsUsingMapper() {
+        session.execute("create table notes(note_id int primary key, content text)");
+
+        MappingManager mappingManager = new MappingManager(session);
+        Mapper<Note> noteMapper = mappingManager.mapper(Note.class);
+
+        if(true) {
+            Note note = new Note();
+            note.setId(11);
+            note.setContent("hello there");
+            noteMapper.save(note);
+        }
+
+        Note note = noteMapper.get(11);
+        assertEquals(11, (int) note.getId());
+        assertEquals("hello there", note.getContent());
+    }
+
+    @Table(keyspace = "dummy", name = "notes")
+    public static class Note {
+        @PartitionKey
+        @Column(name = "note_id")
+        private Integer id;
+
+        @Column(name = "content")
+        private String content;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if(!(o instanceof Note)) {
+                return false;
+            }
+
+            Note other = (Note)o;
+            return Objects.equal(id, other.id) &&
+                    Objects.equal(content, other.content);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(id, content);
+        }
     }
 }
