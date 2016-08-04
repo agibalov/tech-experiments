2b3c10d044f879acb7dd2479987d8f963ebf0450
loki2302 <loki2302@loki2302.me>
2016-08-03 22:53:26 -0400

    Upgraded to Spring Boot 1.4.0.RELEASE

--- microservices-experiment/integration-test/src/test/java/me/loki2302/DummyTest.java
+++ microservices-experiment/integration-test/src/test/java/me/loki2302/DummyTest.java
@@ -4,17 +4,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@IntegrationTest
-@SpringApplicationConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@RunWith(SpringRunner.class)
 public class DummyTest {
     @Value("${ADD_SERVICE_URL}")
     private String addServiceUrl;
