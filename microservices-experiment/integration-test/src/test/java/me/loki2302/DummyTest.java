f1b9774fbafef010eaa42a535c376e5cbc7c9a5f
loki2302 <loki2302@loki2302.me>
2016-08-03 23:17:50 -0400

    Extracted service clients from test code

--- microservices-experiment/integration-test/src/test/java/me/loki2302/DummyTest.java
+++ microservices-experiment/integration-test/src/test/java/me/loki2302/DummyTest.java
@@ -2,86 +2,118 @@ package me.loki2302;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 import static org.junit.Assert.assertEquals;
+import static org.springframework.web.util.UriComponentsBuilder.fromHttpUrl;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @RunWith(SpringRunner.class)
 public class DummyTest {
-    @Value("${ADD_SERVICE_URL}")
-    private String addServiceUrl;
+    @Autowired
+    private AddServiceClient addServiceClient;
 
-    @Value("${SUB_SERVICE_URL}")
-    private String subServiceUrl;
+    @Autowired
+    private SubServiceClient subServiceClient;
 
-    @Value("${CALC_SERVICE_URL}")
-    private String calcServiceUrl;
+    @Autowired
+    private CalcServiceClient calcServiceClient;
 
     @Test
     public void addServiceTest() {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseDTO responseDto = restTemplate.getForObject(
-                addServiceUrl + "/add/{a}/{b}",
-                ResponseDTO.class,
-                2,
-                3);
-
-        assertEquals(2, responseDto.a);
-        assertEquals(3, responseDto.b);
-        assertEquals(5, responseDto.result);
+        assertEquals(5, addServiceClient.add(2, 3));
     }
 
     @Test
     public void subServiceTest() {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseDTO responseDto = restTemplate.getForObject(
-                subServiceUrl + "/sub/{a}/{b}",
-                ResponseDTO.class,
-                2,
-                3);
-
-        assertEquals(2, responseDto.a);
-        assertEquals(3, responseDto.b);
-        assertEquals(-1, responseDto.result);
+        assertEquals(-1, subServiceClient.sub(2, 3));
     }
 
     @Test
     public void calcServiceAddTest() {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseDTO responseDto = restTemplate.getForObject(
-                calcServiceUrl + "/add/{a}/{b}",
-                ResponseDTO.class,
-                2,
-                3);
-
-        assertEquals(2, responseDto.a);
-        assertEquals(3, responseDto.b);
-        assertEquals(5, responseDto.result);
+        assertEquals(5, calcServiceClient.add(2, 3));
     }
 
     @Test
     public void calcServiceSubTest() {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseDTO responseDto = restTemplate.getForObject(
-                calcServiceUrl + "/sub/{a}/{b}",
-                ResponseDTO.class,
-                2,
-                3);
-
-        assertEquals(2, responseDto.a);
-        assertEquals(3, responseDto.b);
-        assertEquals(-1, responseDto.result);
+        assertEquals(-1, calcServiceClient.sub(2, 3));
     }
 
     @Configuration
     @EnableAutoConfiguration
     public static class Config {
+        @Bean
+        public AddServiceClient addServiceClient(@Value("${ADD_SERVICE_URL}") String addServiceUrl) {
+            return new AddServiceClient(addServiceUrl);
+        }
+
+        @Bean
+        public SubServiceClient subServiceClient(@Value("${SUB_SERVICE_URL}") String subServiceUrl) {
+            return new SubServiceClient(subServiceUrl);
+        }
+
+        @Bean
+        public CalcServiceClient calcServiceClient(@Value("${CALC_SERVICE_URL}") String calcServiceUrl) {
+            return new CalcServiceClient(calcServiceUrl);
+        }
+    }
+
+    public static class AddServiceClient {
+        private final String apiRootUrl;
+        private final RestTemplate restTemplate = new RestTemplate();
+
+        public AddServiceClient(String apiRootUrl) {
+            this.apiRootUrl = apiRootUrl;
+        }
+
+        public int add(int a, int b) {
+            String url = fromHttpUrl(apiRootUrl).path("/add/{a}/{b}").buildAndExpand(2, 3).toUriString();
+            ResponseDTO responseDto = restTemplate.getForObject(url, ResponseDTO.class);
+            return responseDto.result;
+        }
+    }
+
+    public static class SubServiceClient {
+        private final String apiRootUrl;
+        private final RestTemplate restTemplate = new RestTemplate();
+
+        public SubServiceClient(String apiRootUrl) {
+            this.apiRootUrl = apiRootUrl;
+        }
+
+        public int sub(int a, int b) {
+            String url = fromHttpUrl(apiRootUrl).path("/sub/{a}/{b}").buildAndExpand(2, 3).toUriString();
+            ResponseDTO responseDto = restTemplate.getForObject(url, ResponseDTO.class);
+            return responseDto.result;
+        }
+    }
+
+    public static class CalcServiceClient {
+        private final String apiRootUrl;
+        private final RestTemplate restTemplate = new RestTemplate();
+
+        public CalcServiceClient(String apiRootUrl) {
+            this.apiRootUrl = apiRootUrl;
+        }
+
+        public int add(int a, int b) {
+            String url = fromHttpUrl(apiRootUrl).path("/add/{a}/{b}").buildAndExpand(2, 3).toUriString();
+            ResponseDTO responseDto = restTemplate.getForObject(url, ResponseDTO.class);
+            return responseDto.result;
+        }
+
+        public int sub(int a, int b) {
+            String url = fromHttpUrl(apiRootUrl).path("/sub/{a}/{b}").buildAndExpand(2, 3).toUriString();
+            ResponseDTO responseDto = restTemplate.getForObject(url, ResponseDTO.class);
+            return responseDto.result;
+        }
     }
 
     public static class ResponseDTO {
