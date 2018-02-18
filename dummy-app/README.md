d3513a6ce18f67754d12ca7b718c8554a8bb0089
agibalov <loki2302@loki2302.me>
2018-02-17 22:06:32 -0500

    Added a dummy Java app

---
+++ dummy-app/README.md
@@ -0,0 +1,4 @@
+# dummy-app
+
+* `./gradlew bootRun` to run at `http://localhost:8080`
+* `./gradlew build` to build the JAR with dependencies. Run with `java -jar ./build/libs/dummy-app-1.0-SNAPSHOT.jar`
---
+++ dummy-app/src/main/java/io/agibalov/App.java
@@ -0,0 +1,23 @@
+package io.agibalov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @RestController
+    public static class DummyController {
+        @GetMapping("/")
+        public String index() {
+            return String.format("Hello there %s!", new Date());
+        }
+    }
+}
