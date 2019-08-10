5d624a995489092bee875ad013e48a520361fd5c
agibalov <andrey.agibalov@gmail.com>
2019-08-10 18:55:16 -0400

    Cleaning up

--- graphql-experiment/frontend/src/app/app.component.ts
+++ graphql-experiment/frontend/src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AllTodosQuery, DeleteTodoMutation, PutTodoMutation, Todo, TodoAddedSubscription } from './todos';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  private allTodosQueryObservableSubscription: Subscription;
+  private todoAddedSubscriptionObservableSubscription: Subscription;
   todos: Todo[];
   mostRecentTodo: Todo = null;
 
@@ -25,13 +28,20 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.allTodosQuery.watch().valueChanges.subscribe(result => {
-      this.todos = result.data.todos;
-    });
+    this.allTodosQueryObservableSubscription = this.allTodosQuery.watch().valueChanges
+      .subscribe(result => {
+        this.todos = result.data.todos;
+      });
 
-    this.todoAddedSubscription.subscribe().subscribe(result => {
-      this.mostRecentTodo = result.data.todoAdded;
-    });
+    this.todoAddedSubscriptionObservableSubscription = this.todoAddedSubscription.subscribe()
+      .subscribe(result => {
+        this.mostRecentTodo = result.data.todoAdded;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.allTodosQueryObservableSubscription.unsubscribe();
+    this.todoAddedSubscriptionObservableSubscription.unsubscribe();
   }
 
   createTodo() {
