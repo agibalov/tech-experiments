1ffd1776fd7d3c18fa71dea05765ad9cdc57e9ac
agibalov <andrey.agibalov@gmail.com>
2021-07-31 18:50:08 -0400

    LABS-1483: save progress

--- sql-performance-experiment/migrations/postgres/V1__Initial.sql
+++ sql-performance-experiment/migrations/postgres/V1__Initial.sql
@@ -20,3 +20,54 @@ create table Users(
     constraint UserAccountIdUsernameAK unique (accountId, username),
     constraint UserEmailAK unique (email)
 );
+
+create procedure MakeTemplateData(users integer)
+language plpgsql
+as $$
+declare
+    userId varchar(64);
+begin
+    create temporary table TemplateUsers(
+        sourcedId varchar(64) not null,
+        dateLastModified timestamp not null,
+        username varchar(256) not null,
+        password varchar(64) not null,
+        givenName varchar(256) not null,
+        familyName varchar(256) not null,
+        role UserRole not null,
+        email varchar(256)
+    );
+
+    for i in 1..users loop
+        userId = gen_random_uuid();
+        insert into TemplateUsers(sourcedId, dateLastModified, username, password,
+            givenName, familyName, role, email)
+        values (userId, '2021-01-01 00:00:00', concat('u-', userId), userId,
+            concat('Given ', userId), concat('Family ', userId), 'student', null);
+    end loop;
+end $$;
+
+create procedure DeleteTemplateData()
+language plpgsql
+as $$
+begin
+    drop table TemplateUsers;
+end $$;
+
+create procedure MakeAccount()
+language plpgsql
+as $$
+declare
+    accountId varchar(64);
+begin
+    accountId = gen_random_uuid();
+
+    insert into Accounts(id, name)
+    values (accountId, concat('Account ', accountId));
+
+    insert into Users(accountId, sourcedId, dateLastModified, username, password,
+        givenName, familyName, role, email)
+    select accountId, sourcedId, dateLastModified, username, password,
+        givenName, familyName, role, email
+    from TemplateUsers;
+end $$;
--- sql-performance-experiment/src/main/java/io/agibalov/PopulatePostgresCommandLineRunner.java
+++ sql-performance-experiment/src/main/java/io/agibalov/PopulatePostgresCommandLineRunner.java
@@ -2,6 +2,7 @@ package io.agibalov;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @Slf4j
@@ -19,6 +20,22 @@ public class PopulatePostgresCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        log.info("todo");
+        log.info("MakeTemplateData");
+        jdbcTemplate.update(
+                "call MakeTemplateData(:users)",
+                new MapSqlParameterSource()
+                        .addValue("users", dataConfiguration.getNumberOfUsersPerAccount()));
+
+        for (int i = 0; i < dataConfiguration.getNumberOfAccounts(); ++i) {
+            log.info("MakeAccount {} of {}", i + 1, dataConfiguration.getNumberOfAccounts());
+            jdbcTemplate.update(
+                    "call MakeAccount()",
+                    new MapSqlParameterSource());
+        }
+
+        log.info("DeleteTemplateData");
+        jdbcTemplate.update(
+                "call DeleteTemplateData()",
+                new MapSqlParameterSource());
     }
 }
